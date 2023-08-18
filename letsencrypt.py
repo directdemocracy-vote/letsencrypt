a0916ba9bd54f37b26b42a789a921e1a437b5b4b
megatol <74714212+megatol@users.noreply.github.com>
2023-08-18 09:07:30 +0200

    Update letsencrypt.py

--- letsencrypt.py
+++ letsencrypt.py
@@ -227,7 +227,7 @@ def main(argv=None):
     key = urllib.quote_plus(f.read())
     f.close()
     domain = 'directdemocracy.vote'
-    subdomains = ['', 'app', 'observer', 'publisher', 'station', 'trustee', 'www']
+    subdomains = ['', 'app', 'notary', 'station', 'judge', 'www']
     for subdomain in subdomains:
         if subdomain != '':
             subdomain += '.'
